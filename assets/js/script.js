4953f8bb9949dc78b60ed7ece2bd912a6f9eb72e
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2021-11-21 15:40:22 -0500

    changed var layout at top of code, fixed random function, organized and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,96 @@
-// Assignment code here
-var pswdText = document.querySelector("#password");
+// // Assignment code here
 var generateBtn = document.querySelector("#generate");
-
-const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const special = ["!@#%^&*()~?"];
-const upCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-const lowCase = ["abcdefghijklmnopqrstuvwxyz"];
-let passlength = [" "]
-let password = " ";
+// possible chars for password
+const nums = "0123456789";
+const special = "!@#%^&*()~?";
+const upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowCase = "abcdefghijklmnopqrstuvwxyz";
+let passLength = "";
+let pswd = "";
 
 function generatePassword() {
-  var passlength = window.prompt("How long would you like your password? (Pick a number between 8 and 128)")
-  
-  var lengthOfPass=parseInt(passlength);
-  if (Number.isNaN(passlength)) {
-    alert("Password lenght must be a number")
+  // debugger
+  // ask for number of characters
+  passLength = window.prompt("How many characters would you like your password? (Choose between 8 and 128)"
+  );
+  var lengthPass = parseInt(passLength);
+  if (Number.isNaN(passLength)) {
+    alert("Invalid entry. Must be a number");
+    return null;
   }
-  if (lengthOfPass >= 8 && isNaN(lengthOfPass) === true && lengthOfPass <= 128);
-  else (lengthOfPass < 8 && lengthOfPass > 128);{
-    alert;("Must be between 8-128")
-  };
+  if (lengthPass < 8 || lengthPass > 128) {
+    alert("Password needs to be larger than 8 but less than 128");
+    return null;
+  }
+  // ask for inclusion of numbers
   var includeNums =
-    window.confirm("Include numbers in your password?. (Select OK for Yes or Cancel for No)")
-  if (includeNums) {
-    lengthOfPass = password.concat(nums)
-  };
-
+    window.confirm("Include numbers in your password?. (Select OK for Yes or Cancel for No)");
+  //ask fro inclusion of special chars
   var includeSpecial =
-    window.confirm("Include special characters?", "Yes or No")
-  if (includeSpecial) {
-    lengthOfPass = password.concat(special)
-  };
+    window.confirm("Include special characters?", "Yes or No");
 
+  // ask for inclusion of Upper case letters
   var includeUpCase =
-    window.confirm("Include Upper Case Letters?", "Yes or No")
-  if (includeUpCase) {
-    lengthOfPass = password.concat(upCase)
-  };
+    window.confirm("Include Upper Case Letters?", "Yes or No");
 
+  //ask for inclusion of lower case letters
   var includeLowCase =
-    window.confirm("Inlcude lowercase letter?", "Yes or No")
-  if (includeLowCase) {
-    lengthOfPass = password.concat(lowCase)
-  };
-  var userINput = {
-    includeNums=includeNums,
-    includeSpecial=includeSpecial,
-    includeUpCase=includeUpCase,
-    includeLowCase=includeLowCase,
+    window.confirm("Inlcude lowercase letter?", "Yes or No");
+  //user must select at least one of the options
+  if (includeNums == false && includeSpecial == false && includeUpCase == false && includeLowCase == false) {
+    alert("You must select atleast one option");
+    return null;
   }
-};
-generateBtn.addEventListener("click",generatePassword);
 
-// Get references to the #generate element
+  var userInput = {
+    includeNums: includeNums,
+    includeSpecial: includeSpecial,
+    includeLowCase: includeLowCase,
+    includeUpCase: includeUpCase,
+    lengthPass: lengthPass,
+  };
+  return combineUserInput(userInput);
+}
+// function to combine input from user
+function combineUserInput(lengthOfPass) {
+  // if (includeNums) {
+  //   lengthOfPass = ranPassword.concat(nums)
+  // };
+  // if (includeSpecial) {
+  //   lengthOfPass = ranPassword.concat(special)
+  // };
+  // if (includeUpCase) {
+  //   lengthOfPass = ranPassword.concat(upCase)
+  // };
+  // if (includeLowCase) {
+  //   lengthOfPass = ranPassword.concat(lowCase)
+  // };
 
-// Write password to the #password input
-// function generatePassword() {
-//   var password = window.alert("You will  be given a series of prompts to tailor your random password")
-//   // ask user number of chracters for password
+  if (lengthOfPass.includeNums == true) {
+    pswd += nums;
+  }
+  if (lengthOfPass.includeSpecial == true) {
+    pswd += special;
+  }
+  if ((lengthOfPass.includeUpCase = true)) {
+    pswd += upCase;
+  }
+  if (lengthOfPass.includeLowCase == true) {
+    pswd += lowCase;
+  }
+// calcute input and use radnom
+  var ranPassword = "";
+  for (var x = 0; x<lengthOfPass.lengthPass; x++) {
+    ranPassword = ranPassword + pswd.charAt(Math.floor(Math.random() * pswd.length));
+  }
+  return ranPassword;
+}
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 
-
-//   if
-
-
-
-
-
-
-
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-
-// }
-// generatePassword();
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
+var passwordText = document.querySelector("#password");
